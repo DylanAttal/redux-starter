b5ff34d12c9c48bac513c2d9ef8afaea6781d0a7
Dylan Attal <attal.dylan@gmail.com>
2022-03-08 16:24:57 -0500

    constants for action types

--- src/reducer.js
+++ src/reducer.js
@@ -9,11 +9,13 @@
 //   {}...
 // ]
 
+import * as actions from './actionTypes'
+
 let lastId = 0
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case 'bugAdded':
+    case actions.BUG_ADDED:
       return [
         ...state,
         {
@@ -22,7 +24,7 @@ export default function reducer(state = [], action) {
           resolved: false,
         },
       ]
-    case 'bugRemoved':
+    case actions.BUG_REMOVED:
       return state.filter((bug) => bug.id !== action.payload.id)
     default:
       return state
