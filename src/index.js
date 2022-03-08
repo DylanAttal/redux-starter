a3a8abec9a827816795bd6d161b72f2349c852a5
Dylan Attal <attal.dylan@gmail.com>
2022-03-08 16:42:06 -0500

    can add and resolve a bug

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import * as actions from './actionTypes'
+import { bugAdded, bugResolved } from './actions'
 import store from './store'
 
-const unsubscribe = store.subscribe(() => {
-  console.log('Store changed!', store.getState())
-})
-
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: 'Bug1',
-  },
-})
-
-unsubscribe()
-
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-})
+store.dispatch(bugAdded('Bug 1'))
+store.dispatch(bugResolved(1))
 
 console.log(store.getState())
