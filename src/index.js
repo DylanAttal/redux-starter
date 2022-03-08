62a1661ae6e332ec62000dead84c9b6ab59f090b
Dylan Attal <attal.dylan@gmail.com>
2022-03-08 17:03:46 -0500

    makes custom store to see how createStore works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import { bugAdded, bugResolved } from './actions'
-import store from './store'
+// import { bugAdded, bugResolved } from './actions'
+// import store from './store'
 
-store.dispatch(bugAdded('Bug 1'))
-store.dispatch(bugResolved(1))
+// store.dispatch(bugAdded('Bug 1'))
+// store.dispatch(bugResolved(1))
 
+// console.log(store.getState())
+
+import store from './customStore'
+import * as actions from './actions'
+
+store.subscribe(() => {
+  console.log('Store changed')
+})
+
+store.dispatch(actions.bugAdded('Bug 1'))
 console.log(store.getState())
