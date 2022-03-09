a2dfcf3de389010194b2891d9bd85b459d06cef7
Dylan Attal <attal.dylan@gmail.com>
2022-03-09 14:28:38 -0500

    hooks up redux dev tools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 
 // console.log(store.getState())
 
-import store from './customStore'
+import store from './store'
 import * as actions from './actions'
 
 store.subscribe(() => {
@@ -14,4 +14,8 @@ store.subscribe(() => {
 })
 
 store.dispatch(actions.bugAdded('Bug 1'))
+store.dispatch(actions.bugAdded('Bug 2'))
+store.dispatch(actions.bugAdded('Bug 3'))
+
+store.dispatch(actions.bugResolved(1))
 console.log(store.getState())
