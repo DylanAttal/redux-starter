8ce8bc30b702b19882b8a2ae5d01419b9e31216c
Dylan Attal <attal.dylan@gmail.com>
2022-03-11 14:40:33 -0500

    adds fake error toast middleware

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ const store = configureStore()
 store.dispatch((dispatch, getState) => {
   dispatch({ type: 'bugReceived', bugs: [1, 2, 3] })
 })
+
+store.dispatch({
+  type: 'error',
+  payload: { message: 'An error occurred' },
+})
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import logger from './middleware/logger'
 import reducer from './reducer'
+import toast from './middleware/toast'
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger('console')],
+    middleware: [...getDefaultMiddleware(), logger('console'), toast],
   })
 }
