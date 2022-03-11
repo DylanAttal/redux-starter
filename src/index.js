5015acc1122e242c68cd373c3a4d40276e2c8925
Dylan Attal <attal.dylan@gmail.com>
2022-03-11 14:36:40 -0500

    thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
-import {
-  bugAdded,
-  bugAssignedToUser,
-  bugResolved,
-  getUnresolvedBugs,
-  getBugsByUser,
-} from './store/bugs'
 import configureStore from './store/configureStore'
-import { projectAdded } from './store/projects'
-import { userAdded } from './store/users'
 
 const store = configureStore()
 
-store.subscribe(() => {
-  console.log('Store changed')
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: 'bugReceived', bugs: [1, 2, 3] })
 })
-
-store.dispatch(userAdded({ name: 'User 1' }))
-store.dispatch(userAdded({ name: 'User 2' }))
-
-store.dispatch(projectAdded({ name: 'Project 1' }))
-
-store.dispatch(bugAdded({ description: 'Bug 1' }))
-store.dispatch(bugAdded({ description: 'Bug 2' }))
-store.dispatch(bugAdded({ description: 'Bug 3' }))
-
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
-store.dispatch(bugResolved({ id: 1 }))
-
-const bugsAssignedToUser1 = getBugsByUser(1)(store.getState())
-console.log(bugsAssignedToUser1)
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import logger from './middleware/logger'
 import reducer from './reducer'
 
 export default function () {
-  return configureStore({ reducer, middleware: [logger] })
+  return configureStore({
+    reducer,
+    middleware: [...getDefaultMiddleware(), logger('console')],
+  })
 }
