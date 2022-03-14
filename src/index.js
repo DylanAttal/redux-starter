8beb9361f4da1a74e28553ef91f70845109d71f9
Dylan Attal <attal.dylan@gmail.com>
2022-03-14 16:44:00 -0400

    adds api middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import configureStore from './store/configureStore'
 
 const store = configureStore()
 
-store.dispatch((dispatch, getState) => {
-  dispatch({ type: 'bugReceived', bugs: [1, 2, 3] })
-})
-
 store.dispatch({
-  type: 'error',
-  payload: { message: 'An error occurred' },
+  type: 'apiCallBegan',
+  payload: {
+    url: '/bugs',
+    onSuccess: 'bugsReceived',
+    onError: 'apiRequestFailed',
+  },
 })
