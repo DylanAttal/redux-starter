c49a9fc90af4d08346239bce9ca5747a903d06c3
Dylan Attal <attal.dylan@gmail.com>
2022-03-22 14:51:51 -0400

    group tests

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,19 @@
-it('first test', () => {})
+import { isEven } from './math'
+
+describe('isEven', () => {
+  it('should return true if given an even number', () => {
+    // SUT (system under test)
+    const result = isEven(2)
+
+    // Assertion
+    expect(result).toEqual(true)
+  })
+
+  it('should return false if given an odd number', () => {
+    // SUT (system under test)
+    const result = isEven(1)
+
+    // Assertion
+    expect(result).toEqual(false)
+  })
+})
